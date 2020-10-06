44d558cea8d700efff6d2ceffb79b4ece1778c41
nekonoshiri <gexira.halen.toms@gmail.com>
2020-10-06 15:19:15 +0900

    ページ遷移

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,33 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FirstPage(title: 'First Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class FirstPage extends StatefulWidget {
+  const FirstPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _FirstPageState createState() => _FirstPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _FirstPageState extends State<FirstPage> {
+  String _buttonMessage = 'Go Second Page';
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  void _setMessage(String message) {
+    setState(() {
+      _buttonMessage = message;
     });
   }
 
@@ -44,20 +51,56 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            RaisedButton(
+              onPressed: () async {
+                final messageFromSecond = await Navigator.push(
+                  context,
+                  MaterialPageRoute<String>(
+                    builder: (context) {
+                      return const SecondPage('Go back');
+                    },
+                    fullscreenDialog: true,
+                  ),
+                );
+                _setMessage(messageFromSecond);
+              },
+              child: Text(_buttonMessage),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage(this.messageFromFirst);
+
+  final String messageFromFirst;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(messageFromFirst)),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context, 'Go Second Page Again');
+          },
+          child: Text(messageFromFirst),
+        ),
       ),
     );
   }
